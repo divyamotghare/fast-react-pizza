0d4dc6dc918f342e574a4a81bb6135164d10c462
Divya Motghare <divyamotghare9@gmail.com>
2024-06-21 13:30:39 +0530

    added action in order/orderid path

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Home from "./UI/Home";
-import Error from "./UI/Error";
-import Menu, { menuLoader } from "./features/menu/Menu";
-import Cart from "./features/cart/Cart";
-import CreateOrder, { createOrderAction } from "./features/order/CreateOrder";
-import Order, { orderLoader } from "./features/order/Order";
-import AppLayout from "./UI/AppLayout";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from './UI/Home';
+import Error from './UI/Error';
+import Menu, { menuLoader } from './features/menu/Menu';
+import Cart from './features/cart/Cart';
+import CreateOrder, { createOrderAction } from './features/order/CreateOrder';
+import Order, { orderLoader } from './features/order/Order';
+import AppLayout from './UI/AppLayout';
+import { UpdateOrderAction } from './features/order/UpdateOrder';
 
 const router = createBrowserRouter([
   {
@@ -13,29 +14,30 @@ const router = createBrowserRouter([
     errorElement: <Error />,
     children: [
       {
-        path: "/",
+        path: '/',
         element: <Home />,
       },
       {
-        path: "/menu",
+        path: '/menu',
         element: <Menu />,
         loader: menuLoader,
         errorElement: <Error />,
       },
       {
-        path: "/cart",
+        path: '/cart',
         element: <Cart />,
       },
       {
-        path: "/order/new",
+        path: '/order/new',
         element: <CreateOrder />,
         action: createOrderAction,
       },
       {
-        path: "/order/:orderId",
+        path: '/order/:orderId',
         element: <Order />,
         loader: orderLoader,
         errorElement: <Error />,
+        action: UpdateOrderAction,
       },
     ],
   },
