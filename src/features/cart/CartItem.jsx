b8b209684f3e631c754c134131ba40f5a42c98e3
Divya Motghare <divyamotghare9@gmail.com>
2024-06-21 10:33:33 +0530

    added feature:update quantity and delete button

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,8 +1,14 @@
-import Button from '../../UI/Button';
+import { useSelector } from 'react-redux';
 import { formatCurrency } from '../../utils/helpers';
+import { getCurrentQuantityById } from './cartSlice';
+
+import DeleteItem from './DeleteItem';
+import UpdateItemQuantity from './UpdateItemQuantity';
 
 function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
+
+  const currentQuanitity = useSelector(getCurrentQuantityById(pizzaId));
 
   return (
     <li className="sm-items-center py-3 sm:flex sm:justify-between">
@@ -11,7 +17,11 @@ function CartItem({ item }) {
       </p>
       <div className="flex items-center justify-between sm:gap-6">
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
-        <Button type="small">Delete</Button>
+        <UpdateItemQuantity
+          pizzaId={pizzaId}
+          currentQuantity={currentQuanitity}
+        />
+        <DeleteItem pizzaId={pizzaId} />
       </div>
     </li>
   );
